01a6c0c0cf6bcbec5f231414a41da07b9bc8a447
Keith G <kguydo@gmail.com>
2021-04-11 08:48:18 -0400

    Added some functionality using setenv().  The environment variables are actually being set right now, so I'm going to need to come back to this.

--- ProcessAndSignals/main.c
+++ ProcessAndSignals/main.c
@@ -8,11 +8,54 @@
 
 int main(int argc, const char * argv[]) {
     
-    char environment_name[50];
-    printf("Enter the Environment name: ");
-    scanf("%s", environment_name);
     
-    printf("Environment Value: %s\n", getenv(environment_name));
+    
+    int stayAlive = 1;
+    
+    while(stayAlive == 1)
+    {
+        //Check the value of an environment variable
+        char environment_name[50];
+        printf("Enter the Environment name: ");
+        scanf("%s", environment_name);
+        
+        printf("Environment Value: %s\n", getenv(environment_name));
+        
+        //Set the value of an environment variable
+        char variable_name[15];
+        char variable_value[15];
+        int overwrittenValue;
+        
+        printf("Let's set an environment variable.\n");
+        printf("Enter your Variable Name (Case Sensitive): ");
+        scanf("%s", variable_name);
+        
+        printf("Enter the Variable Value: ");
+        scanf("%s", variable_value);
+        
+        // 1 --> Represents the Overriden of Value.
+        // 0 --> Doesn't override the value
+        printf("Enter the Overriden Value (1 Overrides | 0 Does Not Override): ");
+        scanf("%d", &overwrittenValue);
+        
+        // Returns 0 --> On Success || -1 on failure
+        int status = setenv(variable_name, variable_value, overwrittenValue);
+        
+        if(status == 0)
+        {
+            printf("Environment variable %s Created Successfully!\n", variable_name);
+        }
+        else if(status == -1)
+        {
+            printf("Environment variable not created...\n");
+        }
+        
+        printf("Would you like to Continue (1) or Stop (0)?: ");
+        scanf("%d", &stayAlive);
+        
+    }
+    
+    
     
     return 0;
 }
